f75796027b1cb4dcba37d63f3a443244bda4aba9
Róger <rogercardoso.dev@gmail.com>
2022-09-26 21:35:13 -0300

    Start

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+// START SLICK SLIDER
+$('.img-destaque').slick({
+  infinite: true,
+  adaptiveHeight: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 300,
+  arrows: true,
+  prevArrow: '<div class="slick-prev"><i class="fa-solid fa-angle-left" aria-hidden="true"></i></div>',
+  nextArrow: '<div class="slick-next"><i class="fa-solid fa-angle-right" aria-hidden="true"></i></div>',
+
+});
